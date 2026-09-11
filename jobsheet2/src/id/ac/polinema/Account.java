4d5ed0bf8298637624098049ce3bff72cbb9c6db
helfa25 <calony64@gmail.com>
2026-09-11 08:42:34 +0700

    feat: Add Jobsheet 2 (Class and Objects)

---
+++ jobsheet2/src/id/ac/polinema/Account.java
@@ -0,0 +1,43 @@
+package id.ac.polinema;
+
+public class Account {
+    public String ownerName;
+    public double balance;
+
+    public Account(String ownerName, double balance) {
+        this.ownerName = ownerName;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        balance = balance + amount;
+    }
+
+    public void withdraw(double amount) {
+        balance = balance - amount;
+        if (isOverdrawn()) {
+            balance = balance + amount;
+            System.out.println("Withdrawal rejected: insufficient balance.");
+        }
+    }
+
+    public void printInfo() {
+        System.out.println(ownerName + " - balance: " + balance);
+    }
+
+    public String formatBalance() {
+        return String.format("%,.2f", balance);
+    }
+
+    public boolean isOverdrawn() {
+        return balance < 0;
+    }
+    public void transferTo(Account target, double amount) {
+        if (this.balance >= amount) {
+            this.balance -= amount;
+            target.balance += amount;
+        } else {
+            System.out.println("Transfer rejected: insufficient balance.");
+        }
+    }
+}
---
+++ jobsheet2/src/id/ac/polinema/Main.java
@@ -0,0 +1,30 @@
+package id.ac.polinema;
+
+public class Main {
+    public static void main(String[] args) {
+        // --- OUTPUT LANGKAH 7 ---
+        System.out.println("=== Output Langkah 7 ===");
+        Account[] accounts = new Account[3];
+        accounts[0] = new Account("Nadia", 500000);
+        accounts[0].withdraw(150000);
+        
+        accounts[1] = new Account("Budi", 1000000);
+        
+        accounts[2] = new Account("Sari", 750000);
+        accounts[2].withdraw(250000);
+        
+        for (Account acc : accounts) {
+            acc.printInfo();
+        }
+
+        // --- OUTPUT TUGAS MANDIRI ---
+        System.out.println("\n=== Output Tugas Mandiri (transferTo) ===");
+        Account from = new Account("Nadia", 500000);
+        Account to = new Account("Budi", 200000);
+        
+        from.transferTo(to, 100000);
+        
+        from.printInfo();
+        to.printInfo();
+    }
+}
